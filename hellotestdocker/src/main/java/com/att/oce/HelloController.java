6c700eb7d14d954ddc3c78c29a8d7177a43307d5
oceatt <oceatt@gmail.com>
2017-08-08 13:50:16 -0500

    changes

--- hellotestdocker/src/main/java/com/att/oce/HelloController.java
+++ hellotestdocker/src/main/java/com/att/oce/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Welcome Greetings from ATT!";
+        return "Welcome Greetings from ATT OCE!";
     }
     
 }
