be650638ed196c0d15f016fe96b3151eb9649564
oceatt <oceatt@gmail.com>
2017-08-08 15:08:10 -0500

    changes

--- oce-microservice-archetype/target/classes/archetype-resources/deploy.cmd
+++ oce-microservice-archetype/target/classes/archetype-resources/deploy.cmd
@@ -1,8 +1,8 @@
 docker-machine env --shell cmd default
-@FOR /f "tokens=*" %i IN ('docker-machine env --shell cmd default') DO @%i
+@FOR /f "tokens=*" %%i IN ('docker-machine env --shell cmd default') DO %%i
 
 mvn clean install
 
-kubectl -f create deployment.yaml
+kubectl create -f deployment.yaml
 
-kubectl -f create services.yaml
+kubectl create -f services.yaml
